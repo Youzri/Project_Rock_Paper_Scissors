67160919728ffca8f851e9cdef55455fddbc0fe5
youzri <ahmed.y.alhussainy@gmail.com>
2026-09-11 00:11:18 +0300

    add javascript code write code to grap the user choice

--- script.js
+++ script.js
@@ -103,3 +103,42 @@
 // playGame();
 
 // (humanScore > computerScore) ? console.log("Horray!! You Win!") : console.log("Game Over Computer Wins!");
+
+let humanScore = document.querySelector(".scoreDynamic");
+let computerScore = document.querySelector("scoreDynamicSkynet");
+let currentRouns = document.querySelector(".rnd");
+let humanChoiceIcon = document.querySelector(".humanWeapon");
+let computerChoice = document.querySelector(".skynetWeapon");
+let bannerMessage = document.querySelector(".bannerContainer .text");
+let weapons = document.querySelector(".weapons");
+let humanChoice = "";
+weapons.addEventListener('click', (e) => {
+    let className = e.target.className
+    // console.log(clickedButton);
+    // console.log(e.target.className);
+    switch (className) {
+        case "rockIcon":
+            humanChoice = "rock";
+            // console.log(humanChoice);
+            bannerMessage.textContent = "You Rock!!"
+            
+            break;
+        case "paperIcon":
+            humanChoice = "paper";
+            // console.log(humanChoice);
+            bannerMessage.textContent = "You Paper!!"
+
+
+            break;
+        case "scissorsIcon":
+            humanChoice = "scissors"
+            bannerMessage.textContent = "You Scissors!!"
+            
+
+            break
+        default:
+            bannerMessage.textContent = "Please click on the weapon image to select a weapon!"
+            break;
+    }
+    
+})
